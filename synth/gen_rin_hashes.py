f8f765266128596697fff15b2b3501a80f561d48
MarkDVerhagen <markdverhagen@gmail.com>
2024-07-02 21:55:31 +0200

    new household file, hash scripts, params

---
+++ synth/gen_rin_hashes.py
@@ -0,0 +1,32 @@
+import hashlib
+import json
+import random
+import string
+from synth.params import synth_params
+
+def generate_hashed_id():
+    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+    hashed_id = hashlib.sha256(random_string.encode()).hexdigest()
+    return hashed_id
+
+def generate_n_hashed_ids(n, seed):
+    random.seed(seed)
+    ids = [generate_hashed_id() for _ in range(n)]
+    return ids
+
+# Specify the number of IDs you want to generate
+N = synth_params["N"]
+seed = synth_params["seed"]
+hashed_ids = generate_n_hashed_ids(N, seed=1704)
+
+with open(os.path.join('synth', 'hashed_ids_' + str(N) + '.json'), 'w') as f:
+    json.dump(hashed_ids, f)
+
+
+# Specify the number of IDs you want to generate
+N = synth_params["N_households"]
+seed = synth_params["seed"]
+hashed_ids = generate_n_hashed_ids(N, seed=1704)
+
+with open(os.path.join('synth', 'hashed_hh_ids_' + str(N) + '.json'), 'w') as f:
+    json.dump(hashed_ids, f)
---
+++ synth/params.py
@@ -0,0 +1,5 @@
+synth_params = {
+    "N": 10_000,
+    "N_households": 1_000,
+    "seed": 1704,
+}
