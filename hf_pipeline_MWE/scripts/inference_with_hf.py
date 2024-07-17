6c7518070b3423f62f3ecce17287f0e5e42c132d
varunsatish <varun.satish@princeton.edu>
2024-07-17 09:49:26 -0400

    inference working on folds

--- hf_pipeline_MWE/scripts/inference_with_hf.py
+++ hf_pipeline_MWE/scripts/inference_with_hf.py
@@ -12,21 +12,26 @@ from tqdm import tqdm
 # model arguments from the command line
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_name", type=str, help="model name")
-parser.add_argument("--test_dataset", type=str)
+parser.add_argument("--dataset", type=str)
 parser.add_argument("--fine_tune_method", type=str)
 parser.add_argument("--GPU_util", type=str)
 parser.add_argument("--params", type=str)
+parser.add_argument("--training_folds")
+parser.add_argument("--test_folds")
 args = parser.parse_args()
 
 model_name = args.model_name
-test_dataset = args.test_dataset
+dataset = args.dataset
 fine_tune_method = args.fine_tune_method
 GPU_util = args.GPU_util
 params = args.params
+training_folds = args.training_folds
+first_training_fold, last_training_fold = map(int, training_folds.split("-"))
+test_folds = args.test_folds
 
 project_directory = os.environ.get('project_path') + "/"
 
-def format_fake_data(data):
+def format_salganik_data(data):
 
     ## Taking csv and turning it into HF format
 
@@ -52,29 +57,28 @@ def tokenize_and_prepare(data):
     return tokenizer(data["text"], truncation=True, padding="max_length", max_length=512)
 
 
-if test_dataset == "n_30000":
-    data_to_read = project_directory + "data/fake_data_for_tuning_3cols.csv"
-elif test_dataset == "n_100":
-    data_to_read = project_directory + "data/fake_data_for_tuning_3cols_n_100.csv"
-elif test_dataset == "n_1000":
-    data_to_read = project_directory + "data/fake_data_for_tuning_3cols_subset_n_1000.csv"
-elif test_dataset == "n_5000":
-    data_to_read = project_directory + "data/fake_data_for_tuning_3cols_subset_n_5000.csv"
-elif test_dataset == "n_10000":
-    data_to_read = project_directory + "data/fake_data_for_tuning_3cols_subset_n_100000.csv"
-elif test_dataset == "n_20000":
-    data_to_read = project_directory + "data/fake_data_for_tuning_3cols_subset_n_20000.csv"
+#### SPECIFIYING DATA
+
+if dataset == "salganik":
+    # reading training data 
+    data_to_read = project_directory + "data/salganik_data.csv"
+    test_dataset = pd.read_csv(data_to_read)
+
+    # subsetting data to specified training folds 
+    test_dataset = test_dataset[test_dataset['fold'].between(first_test_fold, last_test_fold)]
+
+    # for keeping track of predictions when saving
+    index_column = test_dataset["rinpersoon"]
+
+    # formatting the salganik data to get it into a format readable by the LLM
+    test_dataset = format_salganik_data(test_dataset)    
+
 else:
     raise Exception("Dataset not recognised")
 
-model_save_name = "-".join([model_name, test_dataset, fine_tune_method, GPU_util, params])
-
+# path to where the fine-tuned model is saved 
+model_save_name = "-".join([model_name, dataset, fine_tune_method, GPU_util, params, "folds", training_folds])
 fine_tuned_directory = project_directory + "fine_tuned_models/" + model_save_name
-
-# formatting dataset
-test_dataset = pd.read_csv(data_to_read)
-test_dataset = format_fake_data(test_dataset)
-
 
 # Load the fine-tuned model and tokenizer
 model = AutoModelForSequenceClassification.from_pretrained(fine_tuned_directory, device_map="auto")
@@ -94,8 +98,14 @@ for sample in tqdm(test_dataset):
 
     prediction_probabilities.append(soft_max_probability)
 
-save_path = project_directory + "output/predictions/" + model_save_name + ".csv"
-prediction_df = pd.DataFrame(prediction_probabilities)
+
+save_path = project_directory + "output/predictions/" + model_save_name + "-predictions-" + "folds-" + test_folds + ".csv"
+
+prediction_df = pd.DataFrame({
+    "rinspersoon": index_column, 
+    "softmax_probabilities": prediction_probabilities
+    })
+
 prediction_df.to_csv(save_path)
 
 
